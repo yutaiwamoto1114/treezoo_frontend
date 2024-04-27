cdc4e2c18ca4eebe2e0c7dcf87f0bb4521ec22b6
Yuta Iwamoto <yutaiwamoto1114@gmail.com>
2024-04-28 02:22:38 +0900

    created right pane ui

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -9,9 +9,12 @@ class MainScreen extends ConsumerWidget {
     // Providerから現在のテーマ取得
     final ThemeData theme = ref.watch(themeProvider);
 
+    // 右ペインの開閉状態を取得
+    final isRightPaneOpen = ref.watch(isRightPaneOpenProvider);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('TreeZoo'),
+        title: const Text('TreeZoo'),
         actions: <Widget>[
           IconButton(
               icon: Icon(Icons.login),
@@ -29,14 +32,34 @@ class MainScreen extends ConsumerWidget {
       ),
       body: Row(
         children: <Widget>[
-          Expanded(
+          const Expanded(
             child: Center(
               child: Text('家系図アプリのメインコンテンツ'),
             ),
           ),
-          RightPane(), // 右ペインを追加
+          isRightPaneOpen
+              ? RightPane()
+              : _buildOpenButton(context, ref), // 右ペインを追加
         ],
       ),
+    );
+  }
+
+  // 右ペインを開閉するボタン
+  Widget _buildOpenButton(BuildContext context, WidgetRef ref) {
+    // 画面右端のすべてがクリック領域になるように、Column > Expanded として縦方向に伸ばす
+    return Column(
+      children: [
+        Expanded(
+          child: IconButton(
+            icon: Icon(Icons.chevron_left), // 開くボタンのアイコン
+            onPressed: () {
+              // 右ペインの開閉状態をtrueにすることで、開かせる
+              ref.read(isRightPaneOpenProvider.notifier).state = true;
+            },
+          ),
+        )
+      ],
     );
   }
 }
--- lib/right_pane.dart
+++ lib/right_pane.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// パネルの開閉状態
-final isPaneOpenProvider = StateProvider<bool>((ref) => true);
+// 右ペインの開閉状態を管理するProvider
+final isRightPaneOpenProvider = StateProvider<bool>((ref) => true);
+
+// 右ペインのサイズを管理するProvider
+final rightPaneWidthProvider = StateProvider<double>((ref) => 300.0); // 初期幅300
 
 // 右ペインウィジェット
+// Providerから状態を取得するのでConsumerWidgetとして定義
 class RightPane extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isPaneOpen = ref.watch(isPaneOpenProvider);
+    final isOpen = ref.watch(isRightPaneOpenProvider); // 開閉状態
+    final currentWidth = ref.watch(rightPaneWidthProvider); // 現在の幅
 
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 300),
-      width: isPaneOpen ? 300 : 0, // 幅調整
-      child: isPaneOpen
-          ? Column(
+    // 将来的にペインの幅を調整できるようにしたい
+    final screenWidth = MediaQuery.of(context).size.width; // 画面幅
+    final maxPaneWidth = screenWidth * 0.4; // 最大幅を画面幅の40%に制限
+
+    // AnimatedContainer: アニメーションをつけるContainer
+    return isOpen
+        ? AnimatedContainer(
+            // duration: アニメーションの長さ
+            duration: Duration(milliseconds: 2300),
+            width: currentWidth, // 幅調整
+            color: Colors.grey[200], // ペインの背景色を設定
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end, // コンテンツを右寄せに設定
               children: [
                 IconButton(
                   icon: Icon(Icons.close),
                   onPressed: () =>
-                      ref.read(isPaneOpenProvider.notifier).state = false,
+                      ref.read(isRightPaneOpenProvider.notifier).state = false,
+                  alignment: Alignment.topRight, // ボタンを右上に配置
                 ),
                 Expanded(
                   child: Center(
@@ -27,8 +41,7 @@ class RightPane extends ConsumerWidget {
                   ),
                 ),
               ],
-            )
-          : SizedBox(),
-    );
+            ))
+        : SizedBox();
   }
 }
